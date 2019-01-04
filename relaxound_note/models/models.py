d66c174ecc70ed2cce1d1a3b09b4b0c25a9009d9
dolly <dolly@techspawn.com>
2019-01-04 15:48:12 +0530

    [IMP]added the decorator in function 	deleted:    relaxound_note/__init__.pyc 	deleted:    relaxound_note/models/__init__.pyc 	deleted:    relaxound_note/models/__pycache__/__init__.cpython-35.pyc 	deleted:    relaxound_note/models/__pycache__/models.cpython-35.pyc 	modified:   relaxound_note/models/models.py 	deleted:    relaxound_note/models/models.pyc

--- relaxound_note/models/models.py
+++ relaxound_note/models/models.py
@@ -7,6 +7,7 @@ class CustomSaleOrder(models.Model):
 	_inherit = "sale.order"
 	is_note = fields.Boolean(compute='onchange_partner_id_credit_1',string="Is Note", default=False)
 
+	@api.multi
 	def onchange_partner_id_credit_1(self):
 
 		if self.partner_id.comment:
