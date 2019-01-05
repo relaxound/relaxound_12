b2d3993185174b27ce2edfb4d95b87d1b465a388
rahelheuser <rahelheuser@zwitscherbox.com>
2019-01-05 10:30:42 +0530

    [IMP]Added the dependencies 	modified:   relaxound_note/__manifest__.py

--- relaxound_note/__manifest__.py
+++ relaxound_note/__manifest__.py
@@ -19,11 +19,11 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': [],
+    'depends': ['sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        # 'views/views.xml',
     ],
 }
