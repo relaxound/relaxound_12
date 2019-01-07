e4d4dff9ed1e305835b52d6c0c6f90dd46cac725
rahelheuser <rahelheuser@zwitscherbox.com>
2019-01-07 10:28:07 +0530

    [IMP]added the xpath in sale order view 	new file:   relaxound_note/__init__.py 	new file:   relaxound_note/__manifest__.py 	new file:   relaxound_note/models/__init__.py 	new file:   relaxound_note/models/__pycache__/__init__.cpython-35.pyc 	new file:   relaxound_note/models/__pycache__/models.cpython-35.pyc 	new file:   relaxound_note/models/models.py 	new file:   relaxound_note/static/description/icon.png 	new file:   relaxound_note/static/description/index.html 	new file:   relaxound_note/static/description/logo.png 	new file:   relaxound_note/views/views.xml

---
+++ relaxound_note/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "relaxound",
+
+    'summary': """
+        Module is developed to manage customers detail""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Techspawn Solutons",
+    'website': "http://www.techspawn.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
