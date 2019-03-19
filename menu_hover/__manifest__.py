b7e610f4efcdccfdd3ab4b0ce3c40ad3095f0627
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-03-19 12:30:49 +0530

    [IMP] Added menu hover module

---
+++ menu_hover/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# module template
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Menu Header Hover',
+    'version': '9.0',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'author': "Odoo Tips",
+    'website': 'http://www.gotodoo.com/',
+    'depends': ['base', 'website'
+                ],
+
+    'images': ['images/main_screenshot.png'],
+    'data': [
+             'views/assets.xml',
+             ],
+    'installable': True,
+    'application': True,
+}
