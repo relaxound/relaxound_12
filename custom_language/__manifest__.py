00df12470eb8f8b1b3550a6dcc995f64061fc7be
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-03-26 10:10:52 +0530

    [IMP]Language Customization based on country

---
+++ custom_language/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# module template
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Custom Language',
+    'version': '12.0',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'author': "Techspawn",
+    'website': 'http://www.Techspawn.com/',
+    'depends': ['sale'],
+
+    'data': [             
+             ],
+    'installable': True,
+    'application': True,
+}
