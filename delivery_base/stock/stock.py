e8136667e1ec861acc2b36d724fd323b043c537c
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-03-26 15:41:50 +0530

    [IMP]Delivery base module added

---
+++ delivery_base/stock/stock.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models,fields
+
+#Trackingnummer aus dem Paket zur Operation hinzufügen
+class stock_move_line(models.Model):
+    _inherit="stock.move.line"
+    tracking_nr=fields.Char("Tracking Number",related='result_package_id.tracking_nr')
+
+#Trackingnummer pro Paket hinzufügen
+class stock_quant_package(models.Model):
+    _inherit="stock.quant.package"
+    tracking_nr=fields.Char("Tracking Number")
