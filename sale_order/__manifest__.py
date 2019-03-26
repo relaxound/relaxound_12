e9be79f47e15877d229d05f401ba82ef7aa3c23e
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-03-26 09:34:09 +0530

    [IMP]Changes in code

--- sale_order/__manifest__.py
+++ sale_order/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale'],
+    'depends': ['base','sales'],
 
     # always loaded
     'data': [
