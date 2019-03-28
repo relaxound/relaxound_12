7b55c2392e1e2d5e14f02118218e3264c87949c6
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-03-28 10:31:05 +0530

    [IMP]update in code

--- product_color_field/model/product_color.py
+++ product_color_field/model/product_color.py
@@ -7,18 +7,4 @@ class ProductColor(models.Model):
     _description ='Color Module'
     name = fields.Char('Name', translate=True, required=True)
     
-#     def name_get(self, cr, uid, ids, context=None):
-#         if context is None:
-#             context = {}
-#         if isinstance(ids, (int, long)):
-#             ids = [ids]
-#         res = []
-#         for record in self.browse(cr, uid, ids, context=context):
-#             res.append((record.id, '%s %s' % (record.number,record.name)))
-#         return res
-#     
-#     def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=100):
-#         result=[]
-#         for color in self.browse(cr, user, self.search(cr, user, ['|',('name','ilike',name),('number','ilike',name)]), context):
-#             result.append((color.id,'%s %s' % (color.number,color.name)))        
-#         return result
+
