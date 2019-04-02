8778814ba048b15502fd2bf9f158d3390fac81d6
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-04-02 10:24:40 +0530

    [IMP]Added Datev Export Cash Automate

---
+++ datev_export_cash_automatic/account/models/account.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2016 Openfellas (http://openfellas.com) All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly advised to contract support@openfellas.com
+#
+##############################################################################
+
+from odoo import models, fields, api
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    auto_export_to_datev = fields.Boolean(string='Export to Datev Automatically', default=False)
