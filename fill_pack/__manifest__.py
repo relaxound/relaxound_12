d7c6043a77e319644df9437e4c782b6cf6c7972f
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-04 11:15:45 +0530

    [IMP]fill pack module added

---
+++ fill_pack/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# module template
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Fill in Packs',
+    'version': '10.0',
+    'category': 'Base',
+    'license': 'AGPL-3',
+    'author': "Odoo Tips",
+    'website': 'http://www.gotodoo.com/',
+    'currency': 'EUR',
+    'price': 19.00,
+    'depends': ['base', 'stock', 'sale_stock'],
+
+    'images': ['images/main_screenshot.png'],
+    'data': [
+             'views/stock_picking_view.xml'
+             ],
+    'installable': True,
+    'application': True,
+}
