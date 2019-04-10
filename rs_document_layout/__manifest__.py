53157beb4bfa9ef9a8b6930d92668007699a9352
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-10 09:51:52 +0530

    [IMP]UPDATE IN CODE

--- rs_document_layout/__manifest__.py
+++ rs_document_layout/__manifest__.py
@@ -9,8 +9,8 @@
     'sequence': 150,
     "init_xml": [],
     'data': [
-            "data/data.xml",
-            "report/inherited_layouts.xml",
+           # "data/data.xml",
+           # "report/inherited_layouts.xml",
             #"report/layout_account_invoice.xml",
             #"report/layout_sale_order.xml",
             #"report/layout_delivery.xml",
