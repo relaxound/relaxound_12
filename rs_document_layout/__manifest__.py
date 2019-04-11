45c50b760ff0fde3163d0e2bbca7a65a8969d213
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-11 15:17:45 +0530

    [IMP]chnage in code

--- rs_document_layout/__manifest__.py
+++ rs_document_layout/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "RS Document Layout",
     "version": "12.0",
-    "depends": ["base","sale","sale_management","stock","account","delivery","product_color_field","l10n_in","fill_pack"],
+    "depends": ["base","sale","sale_management","stock","account","delivery","product_color_field","l10n_in","sale_payment_method","fill_pack"],
     "author": "Level9",
     "category": "Custom Development",
     "description": """    
@@ -9,11 +9,11 @@
     'sequence': 150,
     "init_xml": [],
     'data': [
-            "data/data.xml",
-            "report/inherited_layouts.xml",
-            "report/layout_account_invoice.xml",
-            "report/layout_sale_order.xml",
-            "report/layout_delivery.xml",
+            # "data/data.xml",
+            # #"report/inherited_layouts.xml",
+            # "report/layout_account_invoice.xml",
+            # "report/layout_sale_order.xml",
+            # "report/layout_delivery.xml",
             ],
     'demo_xml': [],
     'installable': True,
