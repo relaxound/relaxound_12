a6e3bf82c64c91eeab13bc473a5ffd38ccb9cc2f
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-12 15:25:47 +0530

    [IMP]blank build

--- rs_document_layout/report/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# from . import report_paperformat
-from . import account_invoice
-# from . import stock_picking
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
