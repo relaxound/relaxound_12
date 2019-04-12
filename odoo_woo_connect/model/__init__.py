ec7b01a6dfe795322e3b795118de38fafb911ef3
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-04-12 16:51:11 +0530

    [IMP]Added product and product tag mapping

--- odoo_woo_connect/model/__init__.py
+++ odoo_woo_connect/model/__init__.py
@@ -22,8 +22,8 @@
 """ 
 All files in current directory imported here
 """
-from . import backend
-# from . import product
+# from . import backend
+from . import product
 # from . import customer
 # from . import shipping_address
 # from . import product_category
