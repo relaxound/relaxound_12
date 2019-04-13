850c272bcdce6a450d6f20ea99c79a61f8dcb410
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-04-13 08:58:39 +0530

    [IMP]updated

--- odoo_woo_connect/__manifest__.py
+++ odoo_woo_connect/__manifest__.py
@@ -59,7 +59,7 @@ This module works very well with latest version of Wordpress v4.4 or later and W
     'data': [
               # 'security/bridge_security.xml',
               # 'security/ir.model.access.csv',
-              # 'views/wp_bridge_view.xml',
+              'views/wp_bridge_view.xml',
               # 'views/product.xml',
               # 'views/customer.xml',
               # 'views/custom_field.xml',
