223963c3e70df4b089cc548a2e444fdb59a345d3
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-17 17:11:02 +0530

    [IMP]change in code

--- prepayment/account/account_payment.py
+++ prepayment/account/account_payment.py
@@ -5,23 +5,23 @@ from odoo import models,api
 class account_payment(models.Model):
     _inherit='account.payment'
 
-    # @api.multi
-    # def post(self):
-    #     res=super(account_payment,self).post()
+    @api.multi
+    def post(self):
+        res=super(account_payment,self).post()
 
-    #     for inv in self.invoice_ids:
-    #         print(inv.state)
-    #         if inv.state!='paid':
-    #             continue            
-    #         #sales= self.env['sale.order'].search([('invoice_ids','in',inv.ids)])
-    #         #sales= self.env['sale.order'].search([('invoice_ids','=',[inv.id])])
-    #         sales= self.env['sale.order'].search([])
-    #         for sa in sales:
-    #             if inv in sa.invoice_ids:                    
-    #                 for picking in sa.picking_ids:
-    #                     print (picking.state)
-    #                     if picking.state=='to_pay':
-    #                         picking.action_payed()    
-    #     return res
+        for inv in self.invoice_ids:
+            print(inv.state)
+            if inv.state!='paid':
+                continue            
+            #sales= self.env['sale.order'].search([('invoice_ids','in',inv.ids)])
+            #sales= self.env['sale.order'].search([('invoice_ids','=',[inv.id])])
+            sales= self.env['sale.order'].search([])
+            for sa in sales:
+                if inv in sa.invoice_ids:                    
+                    for picking in sa.picking_ids:
+                        print (picking.state)
+                        if picking.state=='to_pay':
+                            picking.action_payed()    
+        return res
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:        
--- prepayment/stock/stock.py
+++ prepayment/stock/stock.py
@@ -239,24 +239,24 @@ class stock_picking(models.Model):
         return True
 
     @api.multi
-    def action_assign(self):
-        """ Check availability of picking moves.
-        This has the effect of changing the state and reserve quants on available moves, and may
-        also impact the state of the picking as it is computed based on move's states.
-        @return: True
-        """
-        self.filtered(lambda picking: picking.state == 'draft').action_confirm()
-        moves = self.mapped('move_lines').filtered(lambda move: move.state not in ('draft', 'cancel', 'done'))
-        if not moves:
-            raise UserError('Nothing to check the availability for.')
-        # If a package level is done when confirmed its location can be different than where it will be reserved.
-        # So we remove the move lines created when confirmed to set quantity done to the new reserved ones.
-        package_level_done = self.mapped('package_level_ids').filtered(
-            lambda pl: pl.is_done and pl.state == 'confirmed')
-        package_level_done.write({'is_done': False})
-        moves._action_assign()
-        package_level_done.write({'is_done': True})
-        return True
+    # def action_assign(self):
+    #     """ Check availability of picking moves.
+    #     This has the effect of changing the state and reserve quants on available moves, and may
+    #     also impact the state of the picking as it is computed based on move's states.
+    #     @return: True
+    #     """
+    #     self.filtered(lambda picking: picking.state == 'draft').action_confirm()
+    #     moves = self.mapped('move_lines').filtered(lambda move: move.state not in ('draft', 'cancel', 'done'))
+    #     if not moves:
+    #         raise UserError('Nothing to check the availability for.')
+    #     # If a package level is done when confirmed its location can be different than where it will be reserved.
+    #     # So we remove the move lines created when confirmed to set quantity done to the new reserved ones.
+    #     package_level_done = self.mapped('package_level_ids').filtered(
+    #         lambda pl: pl.is_done and pl.state == 'confirmed')
+    #     package_level_done.write({'is_done': False})
+    #     moves._action_assign()
+    #     package_level_done.write({'is_done': True})
+    #     return True
 
     def _state_get(self):
         '''The state of a picking depends on the state of its related stock.move
