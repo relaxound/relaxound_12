df23cb74932ec7886f20b385d355aebf2bf7bbf6
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-04-17 16:46:50 +0530

    [IMP]Updated

--- odoo_woo_connect/unit/__init__.py
+++ odoo_woo_connect/unit/__init__.py
@@ -29,6 +29,6 @@ from . import product_category_exporter
 from . import product_exporter
 from . import product_tag_exporter
 from . import sale_order_exporter
-from . import sale_order_importer
+# from . import sale_order_importer
 from . import tax_exporter
 from . import product_variation_exporter
