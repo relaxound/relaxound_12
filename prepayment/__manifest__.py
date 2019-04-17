40f8741fea73178d5ef780961bdea6f6a2ebf918
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-17 14:53:51 +0530

    [IMP]update in code

--- prepayment/__manifest__.py
+++ prepayment/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Prepayment",
     "version": "12.0",
-    "depends": ["base","sale","stock","sale_payment_method","account"],
+    "depends": ["base","sale","stock","sale_payment_method","account","purchase"],
     "author": "Level9",
     "website":"https://www.level9.de",
     "category": "Custom Development",
