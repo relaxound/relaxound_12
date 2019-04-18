bdb5488d4e9f4e307ec9f84e8a52291db630ae83
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-19 00:12:03 +0530

    [IMP]change in code

--- prepayment/stock/stock.py
+++ prepayment/stock/stock.py
@@ -78,49 +78,49 @@ class stock_move(models.Model):
         return self
 
 
-    # def _action_confirm(self, merge=True, merge_into=False):
-    #     """ Confirms stock move or put it in waiting if it's linked to another move.
-    #     :param: merge: According to this boolean, a newly confirmed move will be merged
-    #     in another move of the same picking sharing its characteristics.
-    #     """
-    #     move_create_proc = self.env['stock.move']
-    #     move_to_confirm = self.env['stock.move']
-    #     move_waiting = self.env['stock.move']
-    #     move_to_pay = self.env['stock.move']
-    #     to_assign = {}
-    #     for move in self:
-    #         # if the move is preceeded, then it's waiting (if preceeding move is done, then action_assign has been called already and its state is already available)
-    #         if move.move_orig_ids:
-    #             move_waiting |= move
-    #         else:
-    #             if move.procure_method == 'make_to_order':
-    #                 move_create_proc |= move
-    #             else:
-    #                 move_to_confirm |= move
-    #         if move._should_be_assigned():
-    #             key = (move.group_id.id, move.location_id.id, move.location_dest_id.id)
-    #             if key not in to_assign:
-    #                 to_assign[key] = self.env['stock.move']
-    #             to_assign[key] |= move
+    def _action_confirm(self, merge=True, merge_into=False):
+        """ Confirms stock move or put it in waiting if it's linked to another move.
+        :param: merge: According to this boolean, a newly confirmed move will be merged
+        in another move of the same picking sharing its characteristics.
+        """
+        move_create_proc = self.env['stock.move']
+        move_to_confirm = self.env['stock.move']
+        move_waiting = self.env['stock.move']
+        move_to_pay = self.env['stock.move']
+        to_assign = {}
+        for move in self:
+            # if the move is preceeded, then it's waiting (if preceeding move is done, then action_assign has been called already and its state is already available)
+            if move.move_orig_ids:
+                move_waiting |= move
+            else:
+                if move.procure_method == 'make_to_order':
+                    move_create_proc |= move
+                else:
+                    move_to_confirm |= move
+            if move._should_be_assigned():
+                key = (move.group_id.id, move.location_id.id, move.location_dest_id.id)
+                if key not in to_assign:
+                    to_assign[key] = self.env['stock.move']
+                to_assign[key] |= move
 
-    #     # create procurements for make to order moves
-    #     for move in move_create_proc:
-    #         values = move._prepare_procurement_values()
-    #         origin = (move.group_id and move.group_id.name or (move.origin or move.picking_id.name or "/"))
-    #         self.env['procurement.group'].run(move.product_id, move.product_uom_qty, move.product_uom, move.location_id,
-    #                                           move.rule_id and move.rule_id.name or "/", origin,
-    #                                           values)
+        # create procurements for make to order moves
+        for move in move_create_proc:
+            values = move._prepare_procurement_values()
+            origin = (move.group_id and move.group_id.name or (move.origin or move.picking_id.name or "/"))
+            self.env['procurement.group'].run(move.product_id, move.product_uom_qty, move.product_uom, move.location_id,
+                                              move.rule_id and move.rule_id.name or "/", origin,
+                                              values)
 
-    #     move_to_confirm.write({'state': 'confirmed'})
-    #     (move_waiting | move_create_proc).write({'state': 'waiting'})
+        move_to_confirm.write({'state': 'confirmed'})
+        (move_waiting | move_create_proc).write({'state': 'waiting'})
 
-    #     # assign picking in batch for all confirmed move that share the same details
-    #     for moves in to_assign.values():
-    #         moves._assign_picking()
-    #     self._push_apply()
-    #     if merge:
-    #         return self._merge_moves(merge_into=merge_into)
-    #     return self
+        # assign picking in batch for all confirmed move that share the same details
+        for moves in to_assign.values():
+            moves._assign_picking()
+        self._push_apply()
+        if merge:
+            return self._merge_moves(merge_into=merge_into)
+        return self
 
 class stock_picking(models.Model):
     _inherit = 'stock.picking'
@@ -142,18 +142,18 @@ class stock_picking(models.Model):
                     if paid:
                         picking.action_payed()
 
-    @api.multi
-    def action_confirm(self):
-        self.mapped('package_level_ids').filtered(lambda pl: pl.state == 'draft' and not pl.move_ids)._generate_moves()
-        # call `_action_confirm` on every draft move
-        self.mapped('move_lines') \
-            .filtered(lambda move: move.state == 'draft') \
-            ._action_confirm()
-        # call `_action_assign` on every confirmed move which location_id bypasses the reservation
-        self.filtered(lambda picking: picking.location_id.usage in (
-        'supplier', 'inventory', 'production') and picking.state == 'confirmed') \
-            .mapped('move_lines')._action_assign()
-        return True
+    # @api.multi
+    # def action_confirm(self):
+    #     self.mapped('package_level_ids').filtered(lambda pl: pl.state == 'draft' and not pl.move_ids)._generate_moves()
+    #     # call `_action_confirm` on every draft move
+    #     self.mapped('move_lines') \
+    #         .filtered(lambda move: move.state == 'draft') \
+    #         ._action_confirm()
+    #     # call `_action_assign` on every confirmed move which location_id bypasses the reservation
+    #     self.filtered(lambda picking: picking.location_id.usage in (
+    #     'supplier', 'inventory', 'production') and picking.state == 'confirmed') \
+    #         .mapped('move_lines')._action_assign()
+    #     return True
 
     def action_payed(self):
         self.mapped('package_level_ids').filtered(lambda pl: pl.state == 'to_pay' and not pl.move_ids)._generate_moves()
