5db01ca31ee3f4490b967f77af786a02171da71f
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-22 22:22:28 +0530

    [IMP]update in code

--- request_quote/__manifest__.py
+++ request_quote/__manifest__.py
@@ -19,7 +19,7 @@
     'category': 'Sale/Crm',
     'version': '1.1',
 
-    'depends': ['base','crm', 'sale', 'sale_management', 'sale_crm'],
+    'depends': ['base','crm', 'sale', 'sale_management', 'sale_crm', 'website_sale'],
 
 
     'data': [
