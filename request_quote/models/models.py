c10d23be497c3cae177ad0cfc7fb343cdbfea8c8
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-22 15:23:18 +0530

    [IMP]change in code

--- request_quote/models/models.py
+++ request_quote/models/models.py
@@ -12,31 +12,31 @@ import odoo.addons.decimal_precision as dp
 class Quote_Lead(models.Model):
     _inherit = 'crm.lead'
 
-    # @api.depends('order_line.price_total')
-    # def _amount_all(self):
-    #     """
-    #     Compute the total amounts of the SO.
-    #     """
-    #     for order in self:
-    #         amount_untaxed = amount_tax = 0.0
-    #         for line in order.order_line:
-    #             amount_untaxed += line.price_subtotal
-    #             # FORWARDPORT UP TO 10.0
-    #             if order.company_id.tax_calculation_rounding_method == 'round_globally':
-    #                 price = line.price_unit * \
-    #                         (1 - (line.discount or 0.0) / 100.0)
-    #                 taxes = line.taxes_id.compute_all(
-    #                     price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id,
-    #                     partner=line.order_id.partner_id)
-    #                 amount_tax += sum(t.get('amount', 0.0)
-    #                                   for t in taxes.get('taxes', []))
-    #             else:
-    #                 amount_tax += line.price_tax
-    #         order.update({
-    #             'amount_untaxed': amount_untaxed,
-    #             'amount_tax': amount_tax,
-    #             'amount_total': amount_untaxed + amount_tax,
-    #         })
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        """
+        Compute the total amounts of the SO.
+        """
+        for order in self:
+            amount_untaxed = amount_tax = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                # FORWARDPORT UP TO 10.0
+                if order.company_id.tax_calculation_rounding_method == 'round_globally':
+                    price = line.price_unit * \
+                            (1 - (line.discount or 0.0) / 100.0)
+                    taxes = line.taxes_id.compute_all(
+                        price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id,
+                        partner=line.order_id.partner_id)
+                    amount_tax += sum(t.get('amount', 0.0)
+                                      for t in taxes.get('taxes', []))
+                else:
+                    amount_tax += line.price_tax
+            order.update({
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax,
+                'amount_total': amount_untaxed + amount_tax,
+            })
 
     @api.model
     def _default_note(self):
