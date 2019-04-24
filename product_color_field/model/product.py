37754d70f7af8529bd6ad2b64d96e016db9318a1
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-24 21:15:22 +0530

    [IMP]added fill pack

--- product_color_field/model/product.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    color_id = fields.Many2one('product.color', string='Color')
