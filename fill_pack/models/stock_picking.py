b25953297b6cc943d1c9be5ed70951c04d373cad
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-04-24 21:33:42 +0530

    [IMP]added product color field

--- fill_pack/models/stock_picking.py
+++ fill_pack/models/stock_picking.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import models, fields, api
-
-
 class StockPicking(models.Model):
     _inherit = "stock.picking"
     _description = "Stock picking"
