bee156eb96dab65581c1cb0694caf4728304c0f5
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-04-30 16:14:30 +0530

    [IMP]Added Custom Journal

---
+++ custom_journal/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# module template
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Custom Journal',
+    'version': '12.0',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'author': "Techspawn",
+    'website': 'http://www.Techspawn.com/',
+    'depends': ['base', 'account', 'sale'],
+
+    'data': [ 
+            'views/res_partner.xml',       
+             ],
+    'installable': True,
+    'application': True,
+}
