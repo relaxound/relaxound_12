76f6c08163bd54af46ba6aaf7d9bde7b0cdb415e
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-02 12:31:55 +0530

    [IMP]Updated RS document

--- rs_document_layout/report/__init__.py
+++ rs_document_layout/report/__init__.py
@@ -2,3 +2,4 @@
 from . import report_paperformat
 from . import account_invoice
 from . import stock_picking
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- rs_document_layout/report/account_invoice.py
+++ rs_document_layout/report/account_invoice.py
@@ -1,45 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api, fields
-
-class InvoiceJournalField(models.Model):
-    _inherit = 'account.invoice'
-
-
-    journal_id = fields.Many2one('account.journal', string="Journal", required=True)
-    @api.onchange('partner_id')
-    def onchange_partner(self):
-        pro=self.env['res.partner'].search([])
-        pro1=self.env['account.journal'].search([])
-        for item in pro:
-            if item==self.partner_id:
-                if item.customer and not item.is_retailer:
-                    for temp in pro1:
-                        if temp.name=='Export Invoices':
-                            self.update({'journal_id':temp.id})
-
-
-                elif item.customer and item.is_retailer:
-                    for temp in pro1:
-                        if temp.name=='Retail Invoices':
-                            self.update({'journal_id':temp.id})
-
-                else:
-                    self.update({'journal_id':'Tax Invoices'})
-
+from odoo import models, api
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
+    # @api.model
+    # def _get_shipping_address(self):
+    #     # shipping = self.env['res.partner'].search([('parent_id', '=', self.partner_id.id), ('type', '=', 'delivery')])
+    #     shipping = self.env['stock.picking'].search([('origin', '=', self.origin), ('state', '!=', 'cancel')])
+    #     if shipping:
+    #         return shipping[0].partner_id.street + ', ' + shipping[0].partner_id.city + ', ' + shipping[
+    #             0].partner_id.country_id.name
+    #     return False
+
     def _get_shipping_address(self):
         shipping = self.env['res.partner'].search([('parent_id', '=', self.partner_id.id), ('type', '=', 'delivery')])
         so_shipping = self.env['stock.picking'].search([('origin', '=', self.origin), ('state', '!=', 'cancel')])
         if self.origin:
-            if so_shipping:
-                return so_shipping[0].partner_id.street + ', ' + so_shipping[0].partner_id.city + ', ' + so_shipping[0].partner_id.country_id.name
+          if so_shipping:
+            return so_shipping[0].partner_id.street + ', ' + so_shipping[0].partner_id.city + ', ' + so_shipping[0].partner_id.country_id.name
         else:
-            if shipping:
-                return shipping[0].street + ', ' + shipping[0].city + ', ' + shipping[0].country_id.name
+          if shipping:
+            return shipping[0].street + ', ' + shipping[0].city + ', ' + shipping[0].country_id.name
         return False
 
