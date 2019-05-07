7ed416349bccc966f4eb48ab26cc0167f4888389
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-05-07 17:04:24 +0530

    [IMP]CHANGE IN CODE

--- custom_journal/models/journal.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class InvoiceJournalField(models.Model):
-	_inherit = 'account.invoice'
-
-
-	journal_id = fields.Many2one('account.journal', string="Journal", required=True)
-
