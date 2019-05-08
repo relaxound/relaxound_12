0ef08406fdbb81718cc5359bc524aa8e01a5bca3
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-08 10:06:35 +0530

    [IMP]updated

--- rs_document_layout/report/invoice_order.py
+++ rs_document_layout/report/invoice_order.py
@@ -2,6 +2,6 @@ from odoo import models, fields, api, _
 
 
 class OrderLine(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = 'account.invoice.line'
 
     single_unit=fields.Integer(string="Single Unit")
