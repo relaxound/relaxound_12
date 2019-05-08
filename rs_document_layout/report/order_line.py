6bfd963bb0bfc363b54c7107f3a7151691b941dc
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-08 10:56:20 +0530

    [IMP]Updated

--- rs_document_layout/report/order_line.py
+++ rs_document_layout/report/order_line.py
@@ -16,8 +16,7 @@ class OrderLine(models.Model):
 
 	    	elif '80x' in pro or '80X' in pro:
 	    		self.update({'single_unit':self.product_uom_qty*80})
-            else:
-                self.update({'single_unit':self.product_uom_qty})
+            
 
 
 
