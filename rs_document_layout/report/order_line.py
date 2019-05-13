e8603df449c47d5184e7c3668cfb15d3b6060851
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-05-13 11:26:58 +0530

    [IMP]update

--- rs_document_layout/report/order_line.py
+++ rs_document_layout/report/order_line.py
@@ -4,18 +4,21 @@ from odoo import models, fields, api, _
 class OrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    single_unit=fields.Integer(string="Single Unit")
+    single_unit=fields.Float(string="Single Unit")
 
     
     @api.onchange('product_id','product_uom_qty')
     def change_qty(self):
-        pro=self.product_id.name
-        if pro:
-            if '20x' in pro or '20X' in pro:
-                self.update({'single_unit':self.product_uom_qty*20})
+    	pro=self.product_id.name
+    	if pro:
+	    	if '20x' in pro or '20X' in pro:
+	    		self.update({'single_unit':self.product_uom_qty*20})
 
-            elif '80x' in pro or '80X' in pro:
-                self.update({'single_unit':self.product_uom_qty*80})
+	    	elif '80x' in pro or '80X' in pro:
+	    		self.update({'single_unit':self.product_uom_qty*80})
+            else:
+                self.update({'single_unit':self.product_uom_qty})
+
             
 
 
