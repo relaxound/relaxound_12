8707e95488c758928b963c365274bddbe1d0a935
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-14 23:38:25 +0530

    [IMP]Updated

--- custom_sale_order/models/account_invoice.py
+++ custom_sale_order/models/account_invoice.py
@@ -7,7 +7,7 @@ class CustomInvoiceOrder(models.Model):
 
     @api.onchange('partner_id','invoice_line_ids')
     def onchange_invoice(self):
-        if self.partner_id.vat and 'EU' in self.partner_id.property_account_position_id.name::
+        if self.partner_id.vat and 'EU' in self.partner_id.property_account_position_id.name:
             self.invoice_line_ids.update({'invoice_line_tax_ids':None})
             self.update({'tax_line_ids':None})
 
