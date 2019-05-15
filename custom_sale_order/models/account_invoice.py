923772c45f65c5540fa338550721c53625521d1b
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-15 14:42:23 +0530

    [IMP]Updated

--- custom_sale_order/models/account_invoice.py
+++ custom_sale_order/models/account_invoice.py
@@ -7,9 +7,10 @@ class CustomInvoiceOrder(models.Model):
 
     @api.onchange('partner_id','invoice_line_ids')
     def onchange_invoice(self):
-        if self.partner_id.vat and 'EU' in self.partner_id.property_account_position_id.name:
-            self.invoice_line_ids.update({'invoice_line_tax_ids':None})
-            self.update({'tax_line_ids':None})
+        if self.partner_id.vat:
+        	if 'EU' in self.partner_id.property_account_position_id.name:
+	            self.invoice_line_ids.update({'invoice_line_tax_ids':None})
+	            self.update({'tax_line_ids':None})
 
 
 
--- custom_sale_order/models/sale_order.py
+++ custom_sale_order/models/sale_order.py
@@ -7,18 +7,20 @@ class CustomSaleOrder(models.Model):
 
 	@api.onchange('partner_id','order_line')
 	def custom_tax(self):
-		if self.partner_id.vat and 'EU' in self.partner_id.property_account_position_id.name:
-			self.order_line.update({'tax_id':None})
+		if self.partner_id.vat:
+			if 'EU' in self.partner_id.property_account_position_id.name:
+				self.order_line.update({'tax_id':None})
 
 
 	@api.multi
 	def action_view_invoice(self):
 		res=super(CustomSaleOrder,self).action_view_invoice()
-		if self.partner_id.vat and 'EU' in self.partner_id.property_account_position_id.name:
-			res1=self.env['account.invoice'].search([])
-			for cust in res1:
-				if cust.partner_id==self.partner_id:
-					cust.invoice_line_ids.update({'invoice_line_tax_ids':None})
+		if self.partner_id.vat:
+			if 'EU' in self.partner_id.property_account_position_id.name:
+				res1=self.env['account.invoice'].search([])
+				for cust in res1:
+					if cust.partner_id==self.partner_id:
+						cust.invoice_line_ids.update({'invoice_line_tax_ids':None})
 		return res
 
 
