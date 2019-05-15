dc48d85feec8a082c5fe95042deb413e6a6756b9
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-15 17:36:48 +0530

    [IMP]Added banking mandate

---
+++ account_banking_mandate/models/res_partner.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2016 Openfellas (http://openfellas.com) All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly advised to contract support@openfellas.com
+#
+##############################################################################
+
+from odoo import models, fields, api
+
+
+class ResPartnerBank(models.Model):
+    _inherit = 'res.partner'
+
+    mandate_ids = fields.One2many(
+        comodel_name='account.banking.mandate', inverse_name='partner_id',
+        string='Direct Debit Mandates',
+        help='Banking mandates represents an authorization that the bank '
+             'account owner gives to a company for a specific operation')
+
+    mandates_count = fields.Integer(string='# of Mandates', compute='_get_mandates', readonly=True)
+
+    @api.multi
+    def _get_mandates(self):
+        for partner in self:
+            partner.mandates_count = len(set(partner.mandate_ids))
