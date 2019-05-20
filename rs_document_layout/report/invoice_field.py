bff9ccb4824a561ba9cb1904eb1fb254d13e938d
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-20 16:11:33 +0530

    [IMP]Removed other module

--- rs_document_layout/report/invoice_field.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class InvoiceJournal(models.Model):
-    _inherit = 'account.invoice'
-
-    order_by = fields.Many2one('res.partner', string="Order By")
-    order_date = fields.Date(string='Order Date')
-    
--- woo_commerce_ept/models/woo_req_type_ept.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields,models
-
-class woo_req_type_ept(models.Model):
-    _name='woo.req.type.ept'
-    _description='woo.req.type.ept'
-    
-    name=fields.Char('Type')
