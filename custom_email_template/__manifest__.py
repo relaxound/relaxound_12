1d5c2ed46b58edb7c5f7079bf7ad09cb55f4c5db
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-21 17:47:01 +0530

    [IMP]Added email template

---
+++ custom_email_template/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# module template
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Custom Email template',
+    'version': '12.0',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'author': "Techspawn",
+    'website': 'http://www.Techspawn.com/',
+    'depends': ['sale','purchase'],
+
+    'data': [
+            'data/sale_order_template.xml',
+            'data/purchase_order_sample.xml',          
+             ],
+    'installable': True,
+    'application': True,
+}
