bafbca15515f001dc7a0f461141945567fdcf650
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-21 12:29:40 +0530

    [IMP]Updated tax

--- custom_sale_order/models/account_invoice.py
+++ custom_sale_order/models/account_invoice.py
@@ -7,11 +7,15 @@ class CustomInvoiceOrder(models.Model):
 
     @api.onchange('invoice_line_ids')
     def onchange_invoice(self):
-        if self.partner_id.country_id.name=='Germany':
-            taxx=self.env['account.tax'].search([])
-            for item in taxx:
-                if item.name=="19% Umsatzsteuer":
-                    self.invoice_line_ids.update({'invoice_line_tax_ids':item})
+    	if self.invoice_line_ids:
+	        if self.partner_id.country_id.name=='Germany':
+	            taxx=self.env['account.tax'].search([])
+	            for item in taxx:
+	                if item.name=="19% Umsatzsteuer":
+	                    self.invoice_line_ids.update({'invoice_line_tax_ids':item})
+
+	        elif self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
+	        	self.invoice_line_ids.update({'invoice_line_tax_ids':None})
 
 
 
--- custom_sale_order/models/sale_order.py
+++ custom_sale_order/models/sale_order.py
@@ -7,11 +7,15 @@ class CustomSaleOrder(models.Model):
 
 	@api.onchange('order_line')
 	def custom_tax(self):
-		if self.partner_id.country_id.name=='Germany':
-			taxx=self.env['account.tax'].search([])
-			for item in taxx:
-				if item.name=="19% Umsatzsteuer":
-					self.order_line.update({'tax_id':item})
+		if self.order_line:
+			if self.partner_id.country_id.name=='Germany':
+				taxx=self.env['account.tax'].search([])
+				for item in taxx:
+					if item.name=="19% Umsatzsteuer":
+						self.order_line.update({'tax_id':item})
+
+			elif self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
+				self.order_line.update({'tax_id':None})
 
 
 	@api.multi
@@ -25,6 +29,13 @@ class CustomSaleOrder(models.Model):
 					for item in taxx:
 						if item.name=="19% Umsatzsteuer":
 							cust.invoice_line_ids.update({'invoice_line_tax_ids':item})
+
+		elif self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
+			res1=self.env['account.invoice'].search([])
+			for cust in res1:
+				if cust.partner_id==self.partner_id:
+					cust.invoice_line_ids.update({'invoice_line_tax_ids':None})
+
 		return res
 
 
