0f3d571cdbda9f9245618182d2b5f43d0575d29f
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-21 16:22:49 +0530

    [IMP]Updated

--- custom_sale_order/models/account_invoice.py
+++ custom_sale_order/models/account_invoice.py
@@ -15,7 +15,8 @@ class CustomInvoiceOrder(models.Model):
 		                if item.name=="19% Umsatzsteuer":
 		                    self.invoice_line_ids.update({'invoice_line_tax_ids':item})
 
-		        elif self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
+		        elif self.partner_id.country_id and self.partner_id.property_account_position_id:
+		        	if self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
 		        	self.invoice_line_ids.update({'invoice_line_tax_ids':None})
 
 
