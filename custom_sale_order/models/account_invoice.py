ebfa691fc575727a6e592f9743403f119db48c4e
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-22 12:56:42 +0530

    [IMP]Updated

--- custom_sale_order/models/account_invoice.py
+++ custom_sale_order/models/account_invoice.py
@@ -7,17 +7,17 @@ class CustomInvoiceOrder(models.Model):
 
     @api.onchange('invoice_line_ids')
     def onchange_invoice(self):
-    	if self.invoice_line_ids:
-    		if self.partner_id.country_id or self.partner_id.property_account_position_id:
-		        if self.partner_id.country_id.name=='Germany':
-		            taxx=self.env['account.tax'].search([])
-		            for item in taxx:
-		                if item.name=="19% Umsatzsteuer":
-		                    self.invoice_line_ids.update({'invoice_line_tax_ids':item})
+        if self.invoice_line_ids:
+            if self.partner_id.country_id or self.partner_id.property_account_position_id:
+                if self.partner_id.country_id.name=='Germany':
+                    taxx=self.env['account.tax'].search([])
+                    for item in taxx:
+                        if item.name=="19% Umsatzsteuer":
+                            self.invoice_line_ids.update({'invoice_line_tax_ids':item})
 
-		        elif self.partner_id.country_id and self.partner_id.property_account_position_id:
-		        	if self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
-		        	self.invoice_line_ids.update({'invoice_line_tax_ids':None})
+                elif self.partner_id.country_id and self.partner_id.property_account_position_id:
+                    if self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
+                    self.invoice_line_ids.update({'invoice_line_tax_ids':None})
 
 
 
