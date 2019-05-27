992e42877e463fdc8f65b11e6c9c2981dd0194de
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-27 18:18:42 +0530

    [IMP]Updated

--- custom_sale_order/models/__init__.py
+++ custom_sale_order/models/__init__.py
@@ -5,6 +5,7 @@
 
 from . import sale_order
 from . import account_invoice
+from . import account
 
 
 
--- custom_sale_order/models/account_invoice.py
+++ custom_sale_order/models/account_invoice.py
@@ -11,13 +11,21 @@ class CustomInvoiceOrder(models.Model):
             if self.partner_id.country_id or self.partner_id.property_account_position_id:
                 if self.partner_id.country_id.name=='Germany':
                     taxx=self.env['account.tax'].search([])
+                    pro=self.env['account.invoice.tax'].search([])
                     for item in taxx:
                         if item.name=="19% Umsatzsteuer":
-                            self.invoice_line_ids.update({'invoice_line_tax_ids':item})
-
+                            for o, invoice_line in enumerate(self.invoice_line_ids):
+                                invoice_line.update({'invoice_line_tax_ids':item})
+                            for p, res in enumerate(self.tax_line_ids):
+                                if p == o: 
+                                    res.update({'amount_total': (res.base * item.amount) / 100,
+                                                              'name': item.name,
+                                                              'tax_id': item.id,
+                                                              })
                 elif self.partner_id.country_id and self.partner_id.property_account_position_id:
                     if self.partner_id.country_id.name!='Germany' and 'EU' in self.partner_id.property_account_position_id.name:
                         self.invoice_line_ids.update({'invoice_line_tax_ids':None})
+                        self.tax_line_ids.update({'tax_id':None})
 
 
 
