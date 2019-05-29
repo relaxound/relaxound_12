b69d6a02773d5c5de6703cda37e5eeb07a9477d2
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-05-29 11:25:54 +0530

    [IMP]Updated

--- rs_document_layout/report/invoice_order.py
+++ rs_document_layout/report/invoice_order.py
@@ -5,3 +5,12 @@ class OrderLine(models.Model):
     _inherit = 'account.invoice.line'
 
     single_unit=fields.Integer(string="Single Unit")
+
+    @api.onchange('product_id','quantity')
+    def custom_qty(self):
+        if self.product_id.name:
+            if '20x' in self.product_id.name or '20X' in self.product_id.name:
+                self.update({'single_unit':self.quantity*20}) 
+
+            else:
+                self.update({'single_unit':self.quantity})
