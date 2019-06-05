2d7d12e37712508c5a1ad050fde58a3f8cbb6d30
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-06-05 15:02:26 +0530

    [IMP]commented sale order code

--- custom_journal_sample/models/__init__.py
+++ custom_journal_sample/models/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2016-TODAY Serpent Consulting Services Pvt. Ltd.
 # See LICENSE file for full copyright and licensing details.
 
-# from . import partner
-from . import sale_ex
+from . import partner
+# from . import sale_ex
