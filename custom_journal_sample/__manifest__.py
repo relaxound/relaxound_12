3765329aaaa76a9e878b777d43aa7d0347ab0721
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-06-05 12:13:04 +0530

    [IMP]Updated journal module

---
+++ custom_journal_sample/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# module template
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Custom Journal sample',
+    'version': '12.0',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'author': "Techspawn",
+    'website': 'http://www.Techspawn.com/',
+    'depends': ['contacts','sale','account'],
+
+    'data': [             
+             ],
+    'installable': True,
+    'application': True,
+}
