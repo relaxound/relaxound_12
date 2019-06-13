00204174a1661c4aa29d9e36e4cb207eaa24837e
“rahelheuser” <rahelheuser@zwitscherbox.com>
2019-06-13 18:36:11 +0530

    [IMP}Updated

---
+++ custom_payment_term/models/saleperson.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api, _
+
+
+class AccountInvoice(models.Model):
+	_inherit = 'account.invoice'
+
+
+	@api.onchange('partner_id')
+	def payment_term(self):
+		if not self.partner_id.property_payment_term_id:
+			res=self.env['account.payment.term'].search([])
+			for item in res:
+				if item.name=='14 days after receipt of invoice':
+					self.update({'payment_term_id':item.id})
+
+
