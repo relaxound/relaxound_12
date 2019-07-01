dd1f1637ecc6895a6fe95d4828e8058b4c541a85
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2019-07-01 15:19:48 +0530

    [IMP]added the page

--- woo_commerce_ept/models/sale_order.py
+++ woo_commerce_ept/models/sale_order.py
@@ -586,10 +586,11 @@ class sale_order(models.Model):
                 order_ids = order_ids + order_response.get('orders')
                 total_pages = response.headers.get('X-WC-TotalPages')
                 if int(total_pages) >=2:
-                    list3=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
-                    for page in list3:
-                    # for page in range(2,int(total_pages)+1):            
-                        order_ids = order_ids + self.import_all_woo_orders(wcapi,instance,transaction_log_obj,order_status,page)            
+                    # list3=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+                    # for page in list3:
+                    for page in range(2,int(total_pages)+1):            
+                        order_ids = order_ids + self.import_all_woo_orders(wcapi,instance,transaction_log_obj,order_status,page)  
+                return order_ids          
             
             import_order_ids=[]
             
