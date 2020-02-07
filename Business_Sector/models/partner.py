4e4e054a6d6a2cc1040ec400b9e95dd7f80da192
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2020-02-07 15:16:12 +0530

    [IMP]update zip code

--- Business_Sector/models/partner.py
+++ Business_Sector/models/partner.py
@@ -6,20 +6,21 @@ from odoo import models, fields, api, _
 
 
 class BusinessSector(models.Model):
-	_inherit = 'res.partner'
+
+        _inherit = 'res.partner'
 
         # Added one field Business sector with dropdown
 	agent_name = fields.Char('Sale Agent')
         business_sec = fields.Selection([
-        ('sec1', 'Beauty / Wellness'),
-        ('sec2', 'Books'), 
-        ('sec3', 'Gardening'),
-        ('sec4', 'Gastronomy'),
-        ('sec5', 'Art / Culture'),
-        ('sec6', 'Home Living'),
-        ('sec7', 'Misc'),
-        ('sec8', 'GPC'),
-        ('sec9', 'retail store'),], string='Business Sector:')
+                ('sec1', 'Beauty / Wellness'),
+                ('sec2', 'Books'), 
+                ('sec3', 'Gardening'),
+                ('sec4', 'Gastronomy'),
+                ('sec5', 'Art / Culture'),
+                ('sec6', 'Home Living'),
+                ('sec7', 'Misc'),
+                ('sec8', 'GPC'),
+                ('sec9', 'retail store'),], string='Business Sector:')
 
 
         @api.onchange('zip')
