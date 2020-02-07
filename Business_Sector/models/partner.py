708c4e516ee84d6e8233592705b9480022a71773
rahelheuser@zwitscherbox.com <rahelheuser@zwitscherbox.com>
2020-02-07 15:08:55 +0530

    [ADD]zip code with ref of sale agent

--- Business_Sector/models/partner.py
+++ Business_Sector/models/partner.py
@@ -9,7 +9,8 @@ class BusinessSector(models.Model):
 	_inherit = 'res.partner'
 
         # Added one field Business sector with dropdown
-	business_sec = fields.Selection([
+	agent_name = fields.Char('Sale Agent')
+        business_sec = fields.Selection([
         ('sec1', 'Beauty / Wellness'),
         ('sec2', 'Books'), 
         ('sec3', 'Gardening'),
@@ -19,6 +20,25 @@ class BusinessSector(models.Model):
         ('sec7', 'Misc'),
         ('sec8', 'GPC'),
         ('sec9', 'retail store'),], string='Business Sector:')
+
+
+        @api.onchange('zip')
+        def onchange_zip(self):
+                if self.zip != None and self.zip != '':
+                        with open('/home/mansi/Desktop/zip_code.csv', 'r') as csv_file:
+                                csv_obj = csv.reader(csv_file)
+                                print("#### CSV OPENED #########")
+                                all_val = [i for i in csv_obj]
+                                for row in all_val:
+                                        if self.zip in row:
+                                                self.update({'agent_name': row[1]})
+                                # else:
+                                #         self.update({'agent_name': ''})
+
+                elif self.zip == '':
+                        self.update({'agent_name': None})
+                else:
+                        self.update({'agent_name': None})
 
 
 
