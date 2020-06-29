8e5a97092f1ae97a64bb3c65324a0f7ff053e67f
Techspawn Solution <siddharth@techspawn.com>
2020-06-29 10:23:52 +0530

    [ADD] chnage in agent sale note

--- Business_Sector/models/sale_note.py
+++ Business_Sector/models/sale_note.py
@@ -28,7 +28,7 @@ class defaultnote(models.Model):
                 self.update({'note': "Sie werden betreut durch die Agentur Kuhnle\nKontakt: Agentur Kuhnle, Bonwiedenweg 18, 73312 Türkheim\nE-Mail: s.kuhnle@t-online.de"})
 
         elif self.partner_id.agent_name == 'Werner':
-                self.update({'note': "Sie werden betreut durch die Handelsagentur BUCH + RAUM\nKontakt: Thomas Werner, Sonnhart 78, 83131 Nußdorf am Inn\nE-Mail: werner@buch-raum.de"})
+                self.update({'note': "Sie werden betreut durch die Handelsagentur BUCH + RAUM\nKontakt: Thomas Werner, Tulpenweg 9, 83254 Breitbrunn am Chiemsee\nE-Mail: werner@buch-raum.de"})
 
         elif self.partner_id.agent_name == 'DEsignLICIOUS':
                 self.update({'note': "your contact person is Mirjam Swart\nPhone: 0031 6 36429052\nE-Mail: mirjam@designlicious.nl"})
@@ -37,3 +37,4 @@ class defaultnote(models.Model):
                 self.update({'note': "your contact person is Thijs van de Laak and Anne Vendrig\nPhone: 0034 673 820 269\nE-Mail: anne@thelivingconnection.com"})
         else:
                 self.update({'note' :"Bitte beachten Sie, dass wir bei nicht vereinbartem Skontoabzug oder Kürzung des Rechnungsbetrages eine Bearbeitungsgebühr von 10 EUR erheben müssen."})
+
