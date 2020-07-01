65367be7d909bc2adeaca92e8120ce74e72a3477
Techspawn Solution <siddharth@techspawn.com>
2020-07-01 22:51:50 +0530

    [add]add sales agent in cc

--- custom_email_template/models/partner.py
+++ custom_email_template/models/partner.py
@@ -8,34 +8,71 @@ from odoo import models, fields, api, _
 class ResPartner(models.Model):
     _inherit = 'sale.order'
 
-
     @api.multi
     def action_quotation_send(self):
+        emailcc = ''
+
+        if self.partner_id.agent_name == 'Rossmanek':
+            emailcc = 'info@agentur-rossmanek.de'
+
+        elif self.partner_id.agent_name == 'Pforte':
+            emailcc = 'gunther.pforte@t-online.de'
+
+        elif self.partner_id.agent_name == 'Senft':
+            emailcc = 'info@cs-handelsagentur.de'
+
+        elif self.partner_id.agent_name == 'Wirtz':
+            emailcc = 'agenturwirtz@t-online.de'
+
+        elif self.partner_id.agent_name == 'Kuhnle':
+            emailcc = 's.kuhnle@t-online.de'
+
+        elif self.partner_id.agent_name == 'Werner':
+            emailcc = 'werner@buch-raum.de'
+
         '''
         This function opens a window to compose an email, with the edi sale template message loaded by default
         '''
         self.ensure_one()
         ir_model_data = self.env['ir.model.data']
         try:
-            template_id = ir_model_data.get_object_reference('custom_email_template', 'email_template_sample_test11')[1]
-
+            if self.partner_id.agent_name:
+                template_id = ir_model_data.get_object_reference('custom_email_template', 'email_template_sample_test_emailcc')[1]
+            else:
+                template_id = ir_model_data.get_object_reference('custom_email_template', 'email_template_sample_test11')[1]
         except ValueError:
             template_id = False
         try:
             compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
         except ValueError:
             compose_form_id = False
-        ctx = {
-            'default_model': 'sale.order',
-            'default_res_id': self.ids[0],
-            'default_use_template': bool(template_id),
-            'default_template_id': template_id,
-            'default_composition_mode': 'comment',
-            'mark_so_as_sent': True,
-            'custom_layout': "mail.mail_notification_paynow",
-            'proforma': self.env.context.get('proforma', False),
-            'force_email': True
-        }
+
+
+        if self.partner_id.agent_name:
+            ctx = {
+                'default_model': 'sale.order',
+                'default_res_id': self.ids[0],
+                'default_use_template': bool(template_id),
+                'default_template_id': template_id,
+                'default_composition_mode': 'comment',
+                'mark_so_as_sent': True,
+                'custom_layout': "mail.mail_notification_paynow",
+                'proforma': self.env.context.get('proforma', False),
+                'force_email': True,
+                'email_cc': emailcc,
+            }
+        else:
+            ctx = {
+                'default_model': 'sale.order',
+                'default_res_id': self.ids[0],
+                'default_use_template': bool(template_id),
+                'default_template_id': template_id,
+                'default_composition_mode': 'comment',
+                'mark_so_as_sent': True,
+                'custom_layout': "mail.mail_notification_paynow",
+                'proforma': self.env.context.get('proforma', False),
+                'force_email': True,
+            }
         return {
             'type': 'ir.actions.act_window',
             'view_type': 'form',
@@ -46,5 +83,4 @@ class ResPartner(models.Model):
             'target': 'new',
             'context': ctx,
         }
-
 
