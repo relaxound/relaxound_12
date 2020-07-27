d1f3a828bf5b192bfd2d7432ad4a8e751b106c39
Techspawn Solution <siddharth@techspawn.com>
2020-07-27 17:20:47 +0530

    [add]Customisation of edit log notes

---
+++ custom_chatter_box/models/models.py
@@ -0,0 +1,9 @@
+# # -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+# class Inherit(models.Model):
+#     _inherit = 'mail.message'
+
+#     def open_wizard(self):
+#     	import pdb;pdb.set_trace()
