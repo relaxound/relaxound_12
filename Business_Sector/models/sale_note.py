4b63227132c4621444c720603a54d0d6323c0f66
Techspawn Solution <siddharth@techspawn.com>
2020-09-28 10:54:07 +0530

    [ADD] update sale agent text.

--- Business_Sector/models/sale_note.py
+++ Business_Sector/models/sale_note.py
@@ -13,32 +13,31 @@ class defaultnote(models.Model):
 
         if self.partner_id.agent_name == 'Rossmanek':
 
-                self.update({'note': "Sie werden betreut durch die Handelsagentur Rossmanek\nKontakt: Ole Roßmanek, Timms Hoff 7, 22941 Delingsdorf\nE-Mail: info@agentur-rossmanek.de"})
+                self.update({'note': "Ihr persönlicher Ansprechpartner: Handelsagentur Rossmanek\nKontakt: Ole Roßmanek, Timms Hoff 7, 22941 Delingsdorf\nTel. 04532 28 26 331\nE-Mail: info@agentur-rossmanek.de"})
 
         elif self.partner_id.agent_name == 'Pforte':
-                self.update({'note': "Sie werden betreut durch die Agentur Pforte\nKontakt: Agentur Pforte, Sibeliusstraße 3, 30989 Gehrden\nE-Mail: gunther.pforte@t-online.de"})
+                self.update({'note': "Ihr persönlicher Ansprechpartner: Agentur Pforte\nKontakt: Agentur Pforte, Sibeliusstraße 3, 30989 Gehrden\nTel. 05108 6447801\nE-Mail: gunther.pforte@t-online.de"})
 
         elif self.partner_id.agent_name == 'Senft':
-                self.update({'note': "Sie werden betreut durch die Handelsagentur Senft\nKontakt: Christian Senft, Am Rheinberg 19, 55411 Bingen\nE-Mail: info@cs-handelsagentur.de"})
+                self.update({'note': "Ihr persönlicher Ansprechpartner: Handelsagentur Senft\nKontakt: Christian Senft, Am Rheinberg 19, 55411 Bingen\nTel. 01702102456\nE-Mail: info@cs-handelsagentur.de"})
 
         elif self.partner_id.agent_name == 'Wirtz':
-                self.update({'note': "Sie werden betreut durch die Agentur Wirtz\nKontakt: Agentur Wirtz, Tannenweg 23, 41363 Jüchen\nE-Mail: agenturwirtz@t-online.de"})
+                self.update({'note': "Ihr persönlicher Ansprechpartner: Agentur Wirtz\nKontakt: Agentur Wirtz, Tannenweg 23, 41363 Jüchen\nTel. 02165 87 27 50\nE-Mail: agenturwirtz@t-online.de "})
 
         elif self.partner_id.agent_name == 'Kuhnle':
-                self.update({'note': "Sie werden betreut durch die Agentur Kuhnle\nKontakt: Agentur Kuhnle, Bonwiedenweg 18, 73312 Türkheim\nE-Mail: s.kuhnle@t-online.de"})
+                self.update({'note': "Ihr persönlicher Ansprechpartner: Agentur Kuhnle\nKontakt: Agentur Kuhnle, Bonwiedenweg 18, 73312 Türkheim\nTel. 07331 951110\nE-Mail: s.kuhnle@t-online.de "})
 
         elif self.partner_id.agent_name == 'Werner':
-                self.update({'note': "Sie werden betreut durch die Handelsagentur BUCH + RAUM\nKontakt: Thomas Werner, Tulpenweg 9, 83254 Breitbrunn am Chiemsee\nE-Mail: werner@buch-raum.de"})
+                self.update({'note': "Ihr persönlicher Ansprechpartner: Handelsagentur BUCH + RAUM\nKontakt: Thomas Werner, Tulpenweg 9, 83254 Breitbrunn am Chiemsee\nTel. 0172 7238676\nE-Mail: werner@buch-raum.de"})
 
         elif self.partner_id.agent_name == 'DEsignLICIOUS':
-                self.update({'note': "your contact person is Mirjam Swart\nPhone: 0031 6 36429052\nE-Mail: mirjam@designlicious.nl"})
+                self.update({'note': "For any concerns you are welcome to contact your sales agency DEsignLICIOUS\nContact: Mirjam Swart\nE-Mail: mirjam@designlicious.nl\nPhone: (+31) 6 36429052"})
 
         elif self.partner_id.agent_name == 'The Living Connection':
-                self.update({'note': "your contact person is Thijs van de Laak and Anne Vendrig\nPhone: 0034 673 820 269\nE-Mail: anne@thelivingconnection.com"})
+                self.update({'note': "For any concerns you are welcome to contact your sales agency\nThe Living Connection\nContact: Thijs & Anne van de Laak\nE-Mail: anne@thelivingconnection.com\nPhone: 0034 673 820 269"})
 
         elif self.partner_id.agent_name == 'Handelsagentur Schur GbR':
-                self.update({'note': "Sie werden betreut durch die Handelsagentur Schur GbR\nKontakt: Wolfgang Schur, Johann-Ziegler-Str. 14c, 85221 Dachau\nE-Mail: info@ha-schur.com"})
+                self.update({'note': "Ihr persönlicher Ansprechpartner: Handelsagentur Schur GbR\nKontakt: Wolfgang Schur, Johann-Ziegler-Str. 14c, 85221 Dachau\nTel. 08131 333 26 37\nE-Mail: info@ha-schur.com"})
 
         else:
                 self.update({'note' :"Bitte beachten Sie, dass wir bei nicht vereinbartem Skontoabzug oder Kürzung des Rechnungsbetrages eine Bearbeitungsgebühr von 10 EUR erheben müssen."})
-
