da05fb8354383981f734de67408f01018b64c77a
Techspawn Solution <siddharth@techspawn.com>
2020-10-09 09:31:01 +0530

    [ADD]Fix total overdue bugs

--- total_receivable/models/models.py
+++ total_receivable/models/models.py
@@ -6,11 +6,20 @@ from openerp import models, fields, api
 class CustomSaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    new_credit = fields.Monetary(compute='_partner_id_credit', string='Total Receivable', readonly=False)
+    new_overdue = fields.Monetary(compute='_partner_id_overdue', string='Total Overdue', readonly=False)
 
     @api.depends('partner_id')
-    def _partner_id_credit(self):
-
+    def _partner_id_overdue(self):
         for rec in self:
             if rec.partner_id:
-                rec.new_credit = rec.partner_id.credit
+                rec.new_overdue = rec.partner_id.total_overdue
+
+    # Change total receivable code column logic
+    # new_credit = fields.Monetary(compute='_partner_id_credit', string='Total Receivable', readonly=False)
+    #
+    # @api.depends('partner_id')
+    # def _partner_id_credit(self):
+    #     for rec in self:
+    #         if rec.partner_id:
+    #             rec.new_credit = rec.partner_id.credit
+
