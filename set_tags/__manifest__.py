9952bba0102cbc082a41bef7d722f6a1a555927b
Techspawn Solution <siddharth@techspawn.com>
2020-10-21 09:10:57 +0530

    [ADD]Add set tags of multi order issue.

---
+++ set_tags/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Set Tags",
+
+    'summary': """
+        Module is developed to set tags """,
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Techspawn Solutons",
+    'website': "http://www.techspawn.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '12.0',
+    # any module necessary for this one to work correctly
+    'depends': ['sale','account'],
+
+    # always loaded
+    'data': [
+            'wizard/set_tag_wizard.xml',
+    ],
+}
