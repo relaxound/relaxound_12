17c553d9cec7ab43605e5e52562940dbef093621
Techspawn Solution <siddharth@techspawn.com>
2020-10-30 15:04:00 +0530

    [ADD]Fix set tags for contacts.

--- set_tags/models/models.py
+++ set_tags/models/models.py
@@ -5,11 +5,27 @@ class Wizard(models.TransientModel):
     _name = 'sale.wizard'
     _description = "Set Tags wizard"
 
-    tag_ids = fields.Many2one('crm.lead.tag',string='Set Tags')
+    tag_ids = fields.Many2many('crm.lead.tag',string='Set Tags')
 
     @api.multi
     def Add(self):
         active_ids = self._context.get('active_ids')
         orders = self.env['sale.order'].browse(active_ids)
+
         for order in orders:
             order.tag_ids = self.tag_ids
+
+class ContactWizard(models.TransientModel):
+    _name = 'partner.wizard'
+    _description = "Set Tags wizard for contacts"
+
+    category_id = fields.Many2many('res.partner.category',string='Set Tags')
+
+    @api.multi
+    def Add(self):
+
+        active_ids = self._context.get('active_ids')
+        contacts = self.env['res.partner'].browse(active_ids)
+
+        for cont in contacts:
+            cont.category_id = self.category_id
