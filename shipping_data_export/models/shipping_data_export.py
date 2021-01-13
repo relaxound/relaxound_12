2079916af8661e1312f8a136abb6ca76eb2ad9e9
Techspawn Solution <siddharth@techspawn.com>
2021-01-13 17:32:05 +0530

    [ADD]Remove woo orders lido code

--- shipping_data_export/models/shipping_data_export.py
+++ shipping_data_export/models/shipping_data_export.py
@@ -44,8 +44,7 @@ class AccountInvoice(models.Model):
                 invoices = self.env['account.invoice'].search(
                     [('origin', '=', order.name)])
                 _logger.debug("2 ---------------------> %s" % invoices)
-                data = [order.partner_shipping_id and order.partner_shipping_id.name or '' , str(order.is_retailer),order.partner_shipping_id.woo_company_name_ept and order.partner_shipping_id.woo_company_name_ept.name or '',
-, order.partner_shipping_id.street or '', order.partner_shipping_id.street2 or '', order.partner_shipping_id.city or '',
+                data = [order.partner_shipping_id and order.partner_shipping_id.name or '' , str(order.is_retailer), order.partner_shipping_id.company_id and order.partner_shipping_id.company_id.name or '', order.partner_shipping_id.street or '', order.partner_shipping_id.street2 or '', order.partner_shipping_id.city or '',
                         order.partner_shipping_id.state_id.name or '', order.partner_shipping_id.zip or '', order.partner_shipping_id.country_id.name or '',
                         order.partner_shipping_id.email or '', order.partner_invoice_id.name or '', ' ', order.partner_invoice_id.street or '',
                         order.partner_invoice_id.street2 or '', order.partner_invoice_id.city or '', ' ', order.partner_invoice_id.zip or '',
