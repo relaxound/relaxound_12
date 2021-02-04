9d1915f7cbfae46797d81bd4e5cf8a3491310e29
Techspawn Solution <siddharth@techspawn.com>
2021-02-04 15:29:51 +0530

    [ADD]Fix Overview in Sales Module needs to be rearranged.

---
+++ add_tags/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Add Tags",
+
+    'summary': """Odoo Module Deneme""",
+
+    'description': """
+
+    """,
+
+    'author': "USTA",
+    'website': "http://www.techspawn.com",
+
+    'category': 'Test',
+    'version': '0.1',
+
+    'depends': ['base', 'sale'],
+
+    'data': [
+        'views/views.xml',
+    ],
+
+}
