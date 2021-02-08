cb2dcb1157fb4295bfdfe07d10606ae446adcccf
Techspawn Solution <siddharth@techspawn.com>
2021-02-08 14:31:17 +0530

    [ADD]Fix tags for sale orders.

--- add_tags/models/add.py
+++ add_tags/models/add.py
@@ -3,9 +3,30 @@ from odoo import models,api,fields
 class AddTags(models.Model):
     _inherit = 'sale.order'
 
-    category_id_sale = fields.Many2many(related='partner_id.category_id',string="Tags")
+    category_id_sale = fields.Many2many('crm.lead.tag', 'sale_order_tag_rel', 'order_id', 'tag_id', string='Tags')
 
-    # category_id_sale = fields.Many2many('res.partner.category',string="Tags", compute='_add_tags')
+    # @api.multi
+    # @api.depends('partner_id')
+    # def _add_tags(self):
+    #     list_tag_ids = []
+    #
+    #     for rec in self:
+    #         if rec.tag_ids:
+    #             for tag_idss in rec.tag_ids:
+    #                 list_tag_ids.append(tag_idss.id)
+    #
+    #         # By using char field
+    #         # rec.category_id_sale = ','.join([x.name for x in list_tag_ids])
+    #
+    #         rec.category_id_sale = [(6, 0, list_tag_ids)]
+
+
+
+
+
+
+
+
 
     # @api.multi
     # @api.depends('partner_id')
