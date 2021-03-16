c88318e63e42f616bc79472d91157c5ef4988343
Techspawn Solution <siddharth@techspawn.com>
2021-03-16 13:05:34 +0530

    [ADD]Fix sale order duplicate issue.

--- add_tags/models/add.py
+++ add_tags/models/add.py
@@ -3,20 +3,20 @@ from odoo import models,api,fields
 class AddTags(models.Model):
     _inherit = 'sale.order'
 
-    # category_id_sale = fields.Many2many('crm.lead.tag', related='tag_ids', string="Tags")
+    category_id_sale = fields.Many2many('crm.lead.tag', related='tag_ids', string="Tags")
 
-    category_id_sale = fields.Many2many('crm.lead.tag',string="Tags",readonly=True,compute='_add_tags')
+    # category_id_sale = fields.Many2many('crm.lead.tag',string="Tags",readonly=True,compute='_add_tags')
 
-    @api.multi
-    def _add_tags(self):
-        list_tag_ids = []
-        for rec in self:
-            if rec.tag_ids:
-                for tag_idss in rec.tag_ids:
-                    list_tag_ids.append(tag_idss.id)
+    # @api.multi
+    # def _add_tags(self):
+    #     list_tag_ids = []
+    #     for rec in self:
+    #         if rec.tag_ids:
+    #             for tag_idss in rec.tag_ids:
+    #                 list_tag_ids.append(tag_idss.id)
 
-            # By using char field
-            # rec.category_id_sale = ','.join([x.name for x in list_tag_ids])
+    #         # By using char field
+    #         # rec.category_id_sale = ','.join([x.name for x in list_tag_ids])
 
-            rec.category_id_sale = [(6, 0, list_tag_ids)]
+    #         rec.category_id_sale = [(6, 0, list_tag_ids)]
 
