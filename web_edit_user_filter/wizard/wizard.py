2fc21f6925436eed73cac86344dd0d864fa71b68
Techspawn Solution <siddharth@techspawn.com>
2021-03-17 12:25:43 +0530

    [ADD]Remove web editor code.

--- web_edit_user_filter/wizard/wizard.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-
-class Wizard(models.TransientModel):
-    _name = 'editfilter.wizard'
-    _description = "Edit Filter"
-
-    name = fields.Char(string='Filter Name')
-    user_id = fields.Many2one('res.users', string='User', ondelete='cascade')
