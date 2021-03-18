fe63a15869f45533e3b7e36d9a3d09218b3b57b4
Techspawn Solution <siddharth@techspawn.com>
2021-03-18 08:54:20 +0530

    [ADD]Fix error when trying to duplicate sales orders

--- add_tags/models/add.py
+++ add_tags/models/add.py
@@ -3,42 +3,21 @@ from odoo import models,api,fields
 class AddTags(models.Model):
     _inherit = 'sale.order'
 
-    category_id_sale = fields.Many2many('crm.lead.tag', 'sale_order_tag_rel', 'order_id', 'tag_id', string='Tags')
+    tag_ids = fields.Many2many('crm.lead.tag', 'sale_order_tag_rel', 'order_id', 'tag_id',string='Tags')
+
+
+    # category_id_sale = fields.Many2many('crm.lead.tag',string="Tags",readonly=True,compute='_add_tags')
 
     # @api.multi
-    # @api.depends('partner_id')
     # def _add_tags(self):
     #     list_tag_ids = []
-    #
     #     for rec in self:
     #         if rec.tag_ids:
     #             for tag_idss in rec.tag_ids:
     #                 list_tag_ids.append(tag_idss.id)
-    #
+
     #         # By using char field
     #         # rec.category_id_sale = ','.join([x.name for x in list_tag_ids])
-    #
+
     #         rec.category_id_sale = [(6, 0, list_tag_ids)]
 
-
-
-
-
-
-
-
-
-    # @api.multi
-    # @api.depends('partner_id')
-    # def _add_tags(self):
-    #     for rec in self:
-    #         if rec.partner_id.category_id:
-    #             list_tag_ids = []
-    #             for tag_ids in rec.partner_id.category_id:
-    #                 list_tag_ids.append(tag_ids.id)
-    #
-    #         # By using char field
-    #         # rec.category_id_sale = ','.join([x.name for x in list_tag_ids])
-    #
-    #         rec.category_id_sale = [(6, 0, list_tag_ids)]
-    #
