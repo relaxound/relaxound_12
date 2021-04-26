480afa3721ad41f28df5ed5fa03398be38ff7c70
Pooja <poojab@techspawn.co>
2021-04-26 10:58:55 +0530

    [ADD]Added comment in sum_qty module

--- sum_qty/models/sale_order.py
+++ sum_qty/models/sale_order.py
@@ -3,7 +3,7 @@
 
 from odoo import models, fields, api, _
 
-
+#model used for sum the orderline values
 class SaleOrder(models.Model):
     _inherit = "sale.order"
     _description = "Sale Order Is Sent"
