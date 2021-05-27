7ab6f01401e30e5c634d287a1f53d2533862393b
Techspawn Solution <siddharth@techspawn.com>
2021-05-27 11:46:36 +0530

    [ADD] test bank account issue

--- custom_journal_sample/models/sale_ex.py
+++ custom_journal_sample/models/sale_ex.py
@@ -17,6 +17,8 @@ class sale_order_fun(models.Model):
 			for item in pro2:
 				if item.acc_number=='DE37 1002 0890 0026 6861 30':
 					inv_id.update({'partner_bank_id':item.id})
+					print("---1----")
+					print("---2",item.acc_number)
 
 
 		elif self.name[0]=='0' or self.name[0]=='1' or self.name[0]=='2' or self.name[0]=='3' or self.name[0]=='4' or self.name[0]=='5' or self.name[0]=='6' or self.name[0]=='7' or self.name[0]=='8' or self.name[0]=='9':
@@ -26,5 +28,7 @@ class sale_order_fun(models.Model):
 			for item in pro2:
 				if item.acc_number=='Paypal':
 					inv_id.update({'partner_bank_id':item.id})
+					print("---11----")
+					print("-----22-----",item.acc_number)
 		return inv_id
 
