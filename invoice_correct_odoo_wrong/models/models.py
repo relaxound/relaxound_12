f8a16a4f45e4178debca1880325b007b71ae66c2
Techspawn Solution <siddharth@techspawn.com>
2021-07-03 15:29:20 +0530

    [ADD] fix update new price list

--- invoice_correct_odoo_wrong/models/models.py
+++ invoice_correct_odoo_wrong/models/models.py
@@ -12,3 +12,21 @@ class CustomInvoiceCorrect(models.Model):
             obj._compute_price()
         return vals_list
         # return super(CustomInvoiceCorrect,self).invoice_correct(vals_list)
+
+class UpdateNewPricelist(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def new_pricelist(self):
+        # if self.name == '1943':
+        #     self.property_product_pricelist = self.env['product.pricelist'].search([('id', '=', '722')])
+        #     print("New pricelist=================>", self.property_product_pricelist.name)
+        # for i in self:
+        #     if i.customer == True and i.supplier == False and i.is_retailer == False and i.property_product_pricelist.name == "Public Pricelist":
+        #         i.property_product_pricelist = self.env['product.pricelist'].search([('id','=','722')])
+        #         print("New pricelist=================>",i.property_product_pricelist.name)
+        # all_customers =[]
+        all_customers = self.env['res.partner'].search([])
+        for i in all_customers:
+            if i.customer == True and i.supplier == False and i.is_retailer == False and i.property_product_pricelist.name == "Public Pricelist":
+                i.property_product_pricelist = self.env['product.pricelist'].search([('id','=','824')])
