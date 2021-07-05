cd25f80cecbb6ca19e79d9d18d24ba854631cd67
Techspawn Solution <siddharth@techspawn.com>
2021-07-05 15:58:47 +0530

    fix 3 line pricelist

--- invoice_correct_odoo_wrong/models/models.py
+++ invoice_correct_odoo_wrong/models/models.py
@@ -18,17 +18,20 @@ class UpdateNewPricelist(models.Model):
     _inherit = 'res.partner'
 
     def new_pricelist(self):
-        all_customers = self.env['res.partner'].search([])
-        error_msg1 = _('all customer====== (%s)') % (all_customers)
-        _logger.info(error_msg1)
-        for i in all_customers:
-            try:
-                if i.customer == True and i.supplier == False and i.is_retailer == False and i.property_product_pricelist.name == "Public Pricelist":
-                    change_pricelist =self.env['product.pricelist'].search([('id','=',824)]).id
-                    i.property_product_pricelist = change_pricelist
-                    error_msg = _('property_product_pricelist====== (%s)') % (i.property_product_pricelist.name)
-                    _logger.info(error_msg)
-            except Exception:
-                error_msg2 = _('INVALID ID====== (%s)') % (i)
-                _logger.info(error_msg2)
-                pass
+        list = [347596, 349835, 463928, 368086, 368182, 368525, 455778, 455779, 472348, 369809, 370066, 370067, 370121, 370245, 370403, 370404, 470278, 441960, 386198, 386549, 386550, 387626, 389397, 499561, 499563, 499562, 349836, 349837, 399111, 409010, 345513, 345514, 345515, 471223, 471224, 345507, 345508, 489185, 489186, 457599, 457601, 457602, 457603, 457600, 457604, 345509, 345510, 345511, 345512, 489609, 489610]
+        for j in list:
+            p = int(j)
+            all_customers = self.env['res.partner'].search([('id','=',p)])
+            error_msg1 = _('all customer====== (%s)') % (all_customers)
+            _logger.info(error_msg1)
+            for i in all_customers:
+                try:
+                    if i.customer == True and i.supplier == False and i.is_retailer == False and i.property_product_pricelist.name == "Public Pricelist":
+                        change_pricelist =self.env['product.pricelist'].search([('id','=',824)]).id
+                        i.property_product_pricelist = change_pricelist
+                        error_msg = _('property_product_pricelist====== (%s)') % (i.property_product_pricelist.name)
+                        _logger.info(error_msg)
+                except Exception:
+                    error_msg2 = _('INVALID ID====== (%s)') % (i)
+                    _logger.info(error_msg2)
+                    pass
